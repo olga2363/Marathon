20ea6587d492e6835aaf0fcd7cdc2aeb6744302e
olga2363 <okhomenko2363@gmail.com>
2023-01-23 13:30:39 +0200

    added slider

--- js/program.js
+++ js/program.js
@@ -10,6 +10,29 @@ $(function () {
 
     });
 
+    $(document).ready(function(){
+        $('.slider__content').slick({
+            arrows: true,
+            dots: true,
+            adaptiveHeight: true,
+            slidesToShow: 1,
+            speed: 1000,
+            // autoplay: true,
+            autoplaySpeed: 3000,
+            pauseOnFocus: true,
+            pauseOnHover: true,
+            pauseOnDotsHover: true,
+            // centerMode: true,
+            // variableWidth:true,
+
+            // centerPadding: 50px,
+
+
+            mobileFirst: true,
+        });
+    });
+
+
     // $('.info-slider, .clients-slider').slick({
     //     fade: true,
     //     prevArrow: '<svg class="slick-left" width="41" height="8" viewBox="0 0 41 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.646446 3.64645C0.451183 3.84171 0.451183 4.15829 0.646446 4.35355L3.82843 7.53553C4.02369 7.7308 4.34027 7.7308 4.53553 7.53553C4.7308 7.34027 4.7308 7.02369 4.53553 6.82843L1.70711 4L4.53553 1.17157C4.7308 0.976311 4.7308 0.659728 4.53553 0.464466C4.34027 0.269204 4.02369 0.269204 3.82843 0.464466L0.646446 3.64645ZM41 3.5L1 3.5V4.5L41 4.5V3.5Z" fill="#303030" /></svg>',
